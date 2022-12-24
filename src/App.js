56f9ef442cc69198406d1c747859bd7b5fca8154
Leo Loureiro <leolou.90@gmail.com>
2022-12-23 18:59:21 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () =>{
+  return(
+    <div className='app'>
+      <div className ='gradient-bg'></div>
+      <div className='white-bg'></div>
+    </div>
+  )
+}
+
+export default App;
